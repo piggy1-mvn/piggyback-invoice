69de5e3f7ffa4273276b67ac7e792af9e29b09fe
Rajan Gupta <rajan0493@gmail.com>
2019-11-01 22:10:51 +0800

    Jasypt encryption

--- src/main/java/com/incentives/piggyback/invoice/InvoiceApplication.java
+++ src/main/java/com/incentives/piggyback/invoice/InvoiceApplication.java
@@ -1,5 +1,6 @@
 package com.incentives.piggyback.invoice;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableEncryptableProperties
 public class InvoiceApplication {
 
     public static void main(String[] args) {
